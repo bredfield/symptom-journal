9a525c1cb2b254bdb239825c37a7847c777a5b01
Ben Redfield <bredfield@gmail.com>
2017-12-30 16:47:13 -0500

    Initial commit. Working baseline.

---
+++ log.py
@@ -0,0 +1,248 @@
+import argparse
+import datetime
+import json
+import os
+import shutil
+import sys
+
+
+"""
+JOURNAL FORMAT (JSON)
+
+{
+    "patient": "Patient name"
+    "journal": [{
+        date": "MM-DD-YYYY",
+        "symptoms": [{
+            "name": "Symptom name"
+            "value": 0-10
+        }],
+        "note": "notable items from the day"
+    }]
+}
+"""
+
+
+DATE_FORMAT = '%m-%d-%Y'
+
+
+def read_journal(journal_path):
+    """Reads the journal at the specified path. If a journal doesn't exist,
+    creates a new one after prompting the patient's name.
+
+    args:
+        journal_path (str): path to journal file to read
+
+    returns:
+        dict: json journal contents
+    """
+    try:
+        with open(journal_path, 'rb') as infile:
+            return json.load(infile)
+    except Exception as e:
+        pass
+
+    # no journal found, start a new one
+    print '\n--- No journal found, let\'s start a new one.'
+    print '> What\'s your full name?'
+    name_input = sys.stdin.readline().rstrip()
+
+    return {
+        'patient': name_input,
+        'journal': []
+    }
+
+
+def get_existing_symptoms(journal):
+    """Given a journal w/ proper format, aggregates all the symptoms
+
+    args:
+        journal (dict)
+
+    returns:
+        [str]: array of symptom names
+    """
+    symptoms = []
+
+    for log in journal['journal']:
+        symptoms.extend([
+            symptom['name'] for symptom in log['symptoms'] if symptom["name"] not in symptoms
+        ])
+
+    return symptoms
+
+
+def get_date():
+    """Prompts the user for the date to log. Defaults to today's date
+
+    returns:
+        datetime: date to log
+    """
+    today = datetime.date.today()
+
+    print "\n> What date is this log for? (leave blank for today [{}])".format(
+        today.strftime(DATE_FORMAT)
+    )
+
+    date_input = sys.stdin.readline().rstrip()
+
+    # parse date, default to today
+    try:
+        return datetime.datetime.strptime(date_input, DATE_FORMAT)
+    except Exception as e:
+        return today
+
+
+def log_symptom(existing_symptoms):
+    """Logs a new symptom via stdin. Constructs a list of symptom options based on existing ones.
+    Will act recursively if input is invalid.
+
+    args:
+        [str]: list of existing symptoms
+
+    returns:
+        dict: symptom
+    """
+    num_existing_symptoms = len(existing_symptoms)
+    new_symptom_option = num_existing_symptoms + 1
+    done_option = new_symptom_option + 1
+
+    for i, existing_symptom in enumerate(existing_symptoms):
+        print '{option}: {name}'.format(
+            option=i+1,
+            name=existing_symptom
+        )
+
+    print '{}: Log new symptom'.format(new_symptom_option)
+    print '{}: Done adding symptoms'.format(done_option)
+    print '> Choose a symptom option from the above list'
+
+    option_input = int(sys.stdin.readline().rstrip())
+
+    # existing symptom
+    if option_input <= num_existing_symptoms:
+        symptom_name = existing_symptoms[option_input - 1]
+    # new symptom (prompt user for name)
+    elif option_input == new_symptom_option:
+        print '> What\'s the name of the symptom you\'d like to log?'
+        symptom_name = sys.stdin.readline().rstrip()
+    # user is done adding symptoms
+    elif option_input == done_option:
+        return None
+    # bad option, ty again
+    else:
+        print "\n[X] Invalid option! Try that again.\n"
+        return log_symptom(existing_symptoms)
+
+    print '> On a scale of 0 to 10, how severe is the symptom?'
+    symptom_value = int(sys.stdin.readline().rstrip())
+
+    if symptom_value < 0 or symptom_value > 10:
+        print "\n[X] Invalid symptom severity! Try that again.\n"
+        return log_symptom(existing_symptoms)
+
+    return {
+        'name': symptom_name,
+        'value': symptom_value
+    }
+
+def log_notes():
+    """Prompts the user to log notes for the entry
+
+    returns:
+        str: note
+    """
+    print '\n> Log notes for today\'s entry (optional). Single line only.'
+    return sys.stdin.readline().rstrip()
+
+
+def write_journal(journal_path, journal, journal_archive_dir):
+    """writes the updated journal to it\'s JSON file path
+
+    args:
+        journal_path (str): path to write
+        journal (dict): updated journal to write
+        journal_archive_dir (str): path to archive directory
+
+    """
+    # write main journal file
+    with open(journal_path, 'wb') as outfile:
+        json.dump(journal, outfile, sort_keys=True, indent=4)
+
+    # and archive for posderity
+    if not os.path.exists(journal_archive_dir):
+        os.makedirs(journal_archive_dir)
+
+    today = datetime.datetime.now().isoformat()
+    journal_archive_path = os.path.join(journal_archive_dir, "archive_{}.json".format(today))
+
+    shutil.copyfile(journal_path, journal_archive_path)
+
+def main():
+    print "============================="
+    print "=== Daily Symptom Journal ==="
+    print "============================="
+    parser = argparse.ArgumentParser('Symptom Journal Log')
+    parser.add_argument(
+        '-j', '--journal',
+        type=str,
+        default='journal.json',
+        help='The path the journal you\'d like to update'
+    )
+    parser.add_argument(
+        '-a', '--archive',
+        type=str,
+        default='journal_archive',
+        help='The journal archive directory path'
+    )
+    parser.add_argument(
+        '-n', '--num-daily-symptoms',
+        type=int,
+        default=5,
+        help='The number of daily symptoms to log'
+    )
+
+    args = parser.parse_args()
+
+    journal = read_journal(args.journal)
+
+    print '\n--- Oh hi, {}.'.format(journal['patient'])
+
+    existing_symptoms = get_existing_symptoms(journal)
+
+    # log symptoms!
+    log = {
+        'date': get_date().strftime(DATE_FORMAT),
+        'symptoms': []
+    }
+
+    while len(log['symptoms']) < args.num_daily_symptoms:
+        print '\n--- Logging symptom {i} of {total}'.format(
+            i=len(log['symptoms']) + 1,
+            total=args.num_daily_symptoms
+        )
+
+        symptom = log_symptom(existing_symptoms)
+
+        # done adding symptoms
+        if not symptom:
+            break
+
+        log['symptoms'].append(symptom)
+
+        # remove symptom from existing options once logged
+        if symptom['name'] in existing_symptoms:
+            existing_symptoms.remove(symptom['name'])
+
+    # prompt user to log an optional note
+    log['notes'] = log_notes()
+
+    journal['journal'].append(log)
+
+    write_journal(args.journal, journal, args.archive)
+
+    print '\n=== Successfully wrote journal entry #{}!'.format(len(journal['journal']))
+
+
+if __name__ == '__main__':
+    main()
---
+++ report.py
@@ -0,0 +1,188 @@
+import argparse
+import datetime
+import json
+from itertools import ifilter
+import os
+
+
+DATE_FORMAT = '%m-%d-%Y'
+
+
+def valid_date(date_string):
+    """validates a user-inputted date string.
+
+    args:
+        date_string (str): properly formatted date string
+
+    returns:
+        datetime: converted datetime object
+    """
+    try:
+        return datetime.datetime.strptime(date_string, DATE_FORMAT).date()
+    except ValueError:
+        raise argparse.ArgumentTypeError('Not a valid date: \'{0}\'.'.format(date_string))
+
+
+def read_journal(journal_path):
+    """Reads the journal at the specified path. If a journal doesn't exist,
+    creates a new one after prompting the patient's name.
+
+    args:
+        journal_path (str): path to journal file to read
+
+    returns:
+        dict: json journal contents
+    """
+    with open(journal_path, 'rb') as infile:
+        journal = json.load(infile)
+
+    # convert string times to datetime objects
+    for log in journal['journal']:
+        log['date'] = datetime.datetime.strptime(log['date'], DATE_FORMAT).date()
+
+    return journal
+
+def parse_report_data(journal, start_date, end_date):
+    """Takes a journal and collects relevant report data
+
+    args:
+        journal (dict): journal object
+
+    returns:
+        (str, []): report text, report plot data
+    """
+    sorted_logs = sorted(journal['journal'], key=lambda x: x['date'])
+    filtered_logs = ifilter(
+        lambda x: x['date'] >= start_date and x['date'] <= end_date,
+        sorted_logs
+    )
+
+    # setup initial data
+    report_text = '=====\nDaily symptom journal for {} ({} - {})\n=====\n\n'.format(
+        journal['patient'],
+        start_date.strftime(DATE_FORMAT),
+        end_date.strftime(DATE_FORMAT),
+    )
+    report_plot_data = {}
+
+    for log in filtered_logs:
+        report_text += '--- {} ---\n'.format(log['date'].strftime(DATE_FORMAT))
+
+        for symptom in log['symptoms']:
+            report_text += '- {}: {}\n'.  format(symptom['name'], symptom['value'])
+
+            symptom_data = (log['date'], symptom['value'])
+            symptom_name = symptom['name']
+
+            if symptom_name in report_plot_data:
+                report_plot_data[symptom_name].append(symptom_data)
+            else:
+                report_plot_data[symptom_name] = [symptom_data]
+
+        if len(log['notes']):
+            report_text += '\n  Notes: {}\n'.format(log['notes'])
+
+        report_text += '\n'
+
+    return report_text, report_plot_data
+
+
+def store_report_text(report_text, report_dir, report_name):
+    """generates a text file from symptom data
+
+    args:
+        report_text (str): report text
+        report_dir: directory to store report in
+        report_name: filename for report
+    """
+    with open(os.path.join(report_dir, '{}.txt'.format(report_name)), 'wb') as outfile:
+        outfile.write(report_text)
+
+
+def store_report_plot(report_plot_data, report_dir, report_name):
+    """generates a plot with symptom data and stores to a png image file
+
+    args:
+        report_plot_data (dict): dict with symptom data
+        report_dir: directory to store report in
+        report_name: filename for report
+    """
+    import matplotlib.pyplot as plt
+    import matplotlib.dates as mdates
+
+    # format x axis date ticks
+    fig, ax = plt.subplots()
+    date_format = mdates.DateFormatter(DATE_FORMAT)
+    ax.xaxis.set_major_formatter(date_format)
+
+    # plot each symptom
+    for symptom_name, symptom_data in report_plot_data.iteritems():
+        plt.plot([p[0] for p in symptom_data], [p[1] for p in symptom_data], label=symptom_name)
+
+    plt.legend()
+
+    # store figure
+    fig.set_size_inches(18.5, 10.5)
+    plt.savefig(
+        os.path.join(report_dir, '{}_plot.png'.format(report_name)),
+        dpi=100
+    )
+
+
+def main():
+    today = datetime.date.today()
+
+    parser = argparse.ArgumentParser('Symptom Journal Report')
+    parser.add_argument(
+        '-j', '--journal',
+        type=str,
+        default='journal.json',
+        help='The path the journal you\'d like to update'
+    )
+    parser.add_argument(
+        '-s', '--start-date',
+        type=valid_date,
+        default=(today - datetime.timedelta(days=30)),
+        help='The start date of the report. Follows the format {}'.format(DATE_FORMAT)
+    )
+    parser.add_argument(
+        '-e', '--end-date',
+        type=valid_date,
+        default=today,
+        help='The start date of the report. Follows the format {}'.format(DATE_FORMAT)
+    )
+    parser.add_argument(
+        '-r', '--reports',
+        type=str,
+        default='reports',
+        help='The directory to store reports'
+    )
+    parser.add_argument(
+        '-p', '--plot',
+        dest='plot',
+        action='store_true'
+    )
+
+    args = parser.parse_args()
+
+    try:
+        journal = read_journal(args.journal)
+    except Exception as e:
+        print '\n [X] Failed to read journal. Error: {}'.format(e)
+
+    report_text, report_plot_data = parse_report_data(journal, args.start_date, args.end_date)
+
+    report_name = '{}_{}'.format(
+        args.start_date.strftime(DATE_FORMAT),
+        args.end_date.strftime(DATE_FORMAT)
+    )
+
+    store_report_text(report_text, args.reports, report_name)
+
+    if args.plot:
+        store_report_plot(report_plot_data, args.reports, report_name)
+
+
+
+if __name__ == '__main__':
+    main()
